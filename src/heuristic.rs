3b0c889fa3ac3662b5beb6e652293dd34730e9c7
Glagan <8447621+Glagan@users.noreply.github.com>
2022-03-14 17:13:21 +0100

    Add tests for manhattan heuristic

--- src/heuristic.rs
+++ src/heuristic.rs
@@ -7,3 +7,17 @@ pub fn manhattan(node: &Node, goal: &Node) -> i32 {
     }
     sum
 }
+
+#[test]
+fn manhattan_one() {
+    let left = vec![1, 2, 3, 4, 5, 6, 7, 8, 0];
+    let right = vec![1, 2, 3, 4, 5, 6, 7, 0, 8];
+    assert_eq!(manhattan(&left, &right), 16)
+}
+
+#[test]
+fn manhattan_two() {
+    let left = vec![10, 20, 15, 10, 5];
+    let right = vec![12, 24, 18, 8, 7];
+    assert_eq!(manhattan(&left, &right), 13)
+}
